109d88d64e7979a25c496897e3699b24bf8fa3bf
Anabel <villalobos1998@gmail.com>
2022-04-04 11:09:39 -0700

    added seeders

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .posts import seed_posts,undo_posts
+from .likes import seed_likes, undo_likes
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -10,6 +12,8 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_posts()
+    seed_likes()
     # Add other seed functions here
 
 
