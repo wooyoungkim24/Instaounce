fb60bd90f23090a97b551eb01b402714c9e29c60
Wooyoung Kim <wooyoungkim24@gmail.com>
2022-04-05 17:16:23 -0400

    pre pull

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -18,6 +18,5 @@ def read_posts():
     return {'posts':[following.to_dict() for following in followings]}
 
 
-@post_routes.route("/", methods =['POST'])
-def create_post():
-    
+# @post_routes.route("/", methods =['POST'])
+# def create_post():
