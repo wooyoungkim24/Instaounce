3b12a88add5916ec023ca823fdfc4dfe0f6785e1
Wanyi <wanyilee0805@gmail.com>
2022-04-04 22:06:18 -0700

    add faker comments

---
+++ app/seeds/comments.py
@@ -0,0 +1,18 @@
+from app.models import db, Comment
+from faker import Faker
+faker = Faker()
+
+def seed_comments():
+  for _ in range(50):
+    comment = Comment(
+      post_id = faker.random_int(min=1, max=20),
+      user_id = faker.random_int(min=1, max=2),
+      content = faker.sentence(),
+    )
+
+    db.session.add(comment)
+    db.session.commit()
+
+def undo_comments():
+  db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
+  db.session.commit()
