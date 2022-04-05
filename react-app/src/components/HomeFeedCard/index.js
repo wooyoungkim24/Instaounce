2039616acf7696888fa73c85389a71652c6144f1
Jacob North <yakenorth@protonmail.com>
2022-04-04 23:23:43 -0400

    styling changes to home feed

--- react-app/src/components/HomeFeedCard/index.js
+++ react-app/src/components/HomeFeedCard/index.js
@@ -3,19 +3,39 @@ import { Link } from 'react-router-dom';
 
 const HomeFeedCard = ({ post }) => {
     const user = post.users
-    console.log(post.image)
+    const images = post.image
+    console.log(post)
     return (
         <div className='home-card'>
             <div className='home-card-header'>
                 <Link to={`/users/${user.id}`} >
-                    <img src={user.profile_image} alt='profile pic' />
+                    <img className='home-card-profile-pic' src={user.profile_image} alt='profile pic' />
                 </Link>
-                <Link>
+                <Link className='home-card-username' to={`/users/${user.id}`}>
                     {user.username}
                 </Link>
             </div>
+            <div className='home-card-images'>
+                <img src={images[0]} alt='post pic' />
+                <i className="fa-solid fa-circle-chevron-left left-arrow"></i>
+                <i className="fa-solid fa-circle-chevron-right right-arrow"></i>
+            </div>
+            <div className='home-card-icon-tray' >
+                <div className='home-card-icon-tray-top-left'>
+                    <span>L.K.C</span>
+                    <span>C.C</span>
+                </div>
+                {images.length > 1 &&
+                    <div class='home-card-icon-tray-dots'>
+                        {images.map((image, index) => (
+                            <i key={index} class="fa-solid fa-circle"></i>
+                        ))}
+                    </div>
+                }
+            </div>
             <div>
-                <img src={post.image[0]} alt='post pic' />
+                <Link to={`/users/${user.id}`} >{user.username}</Link>
+                {post.caption}
             </div>
         </div>
     )
