6604fd40c5bc91ca9e3d11cd184dffa78a87906d
Wooyoung Kim <wooyoungkim24@gmail.com>
2022-04-07 20:55:02 -0400

    finished home feed css

--- react-app/src/components/HomeFeedCard/index.js
+++ react-app/src/components/HomeFeedCard/index.js
@@ -5,6 +5,9 @@ import LikeIcon from '../LikeIcon';
 import { Modal } from '../../context/modal';
 import CommentCard from '../CommentCard';
 // import { useSelector } from 'react-redux';
+import moment from 'moment'
+import { useSelector , useDispatch} from 'react-redux';
+import { createComment } from '../../store/posts';
 
 const HomeFeedCard = ({ post }) => {
     const user = post.users;
@@ -13,8 +16,12 @@ const HomeFeedCard = ({ post }) => {
     const comments = Object.values(post.comments)
     const [currentImage, setCurrentImage] = useState(0);
     const [showModal, setShowModal] = useState(false);
+    const [newComment, setNewComment] = useState("");
+    const dispatch = useDispatch();
     // console.log(post);
-
+    const sessionUser = useSelector(state => {
+        return state.session.user
+    })
     const rightClickHandler = () => {
         if (currentImage !== images.length - 1) {
             setCurrentImage(currentImage + 1);
@@ -44,7 +51,27 @@ const HomeFeedCard = ({ post }) => {
             return `View all ${comments.length} comments`
         }
     }
+    const handleCommentSubmit = async (e) => {
+        e.preventDefault()
 
+        const comment = {
+            user_id: sessionUser.id,
+            post_id: post.id,
+            content: newComment
+        }
+        dispatch(createComment(comment))
+        setNewComment('')
+    }
+    function lastUpdated() {
+        let now = new Date();
+        let updatedAt = new Date(post.updated_at)
+        let difference = (now-updatedAt)/1000/60/60
+        if(difference > 24){
+            return moment(updatedAt).format("MMMM D YYYY")
+        }else{
+            return `${Math.floor(difference)} hours ago`
+        }
+    }
     return (
         <div className='home-card'>
             <div className='home-card-header'>
@@ -68,7 +95,7 @@ const HomeFeedCard = ({ post }) => {
                 <div className='home-card-icon-tray' >
                     <div className='home-card-icon-tray-top-left'>
                         <LikeIcon likes={likes} postId={post.id} />
-                        <div onClick={() => setShowModal(true)}><i className="fa-regular fa-comment fa-flip-horizontal  comment-icon"/></div>
+                        <i onClick={() => setShowModal(true)} className="fa-regular fa-comment fa-flip-horizontal  comment-icon" />
                         {showModal && (
                             <Modal onClose={() => setShowModal(false)}>
                                 <CommentCard post={post} />
@@ -92,13 +119,28 @@ const HomeFeedCard = ({ post }) => {
                 </div>
                 <div className='home-card-view-comments'>
                     {/* <Link to={`/${post.id}/comments`}> */}
-                        <div onClick={() => setShowModal(true)}>{viewComments()}</div>
-                        {showModal && (
-                            <Modal onClose={() => setShowModal(false)}>
-                                <CommentCard post={post} />
-                            </Modal>
-                        )}
+                    <div onClick={() => setShowModal(true)}>{viewComments()}</div>
+                    {showModal && (
+                        <Modal onClose={() => setShowModal(false)}>
+                            <CommentCard post={post} />
+                        </Modal>
+                    )}
                     {/* </Link> */}
+                </div>
+                <div className='lastUpdated'>
+                    {lastUpdated()}
+                </div>
+                <div className='card-comment-input'>
+                    <textarea
+
+                    value = {newComment}
+                    placeholder="Add a comment..."
+                    onChange = {e => setNewComment(e.target.value)}
+                    >
+                    </textarea>
+                    <button disabled ={!newComment} id='post-comment-card-button' onClick={handleCommentSubmit}>
+                        Post
+                    </button>
                 </div>
             </div>
         </div>
