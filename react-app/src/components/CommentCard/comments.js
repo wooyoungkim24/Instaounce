a14ff88db7f5dcf7ad2267dd2e5ca9dbd1681f97
Wooyoung Kim <wooyoungkim24@gmail.com>
2022-04-08 15:11:53 -0400

    comment-modal-css

--- react-app/src/components/CommentCard/comments.js
+++ react-app/src/components/CommentCard/comments.js
@@ -3,30 +3,62 @@ import { Link } from "react-router-dom"
 import { Modal } from '../../context/modal';
 import { EditDeleteComment } from './editComment';
 import { useSelector } from 'react-redux'
+import moment from 'moment'
 
-export const Comments = ({comment, post}) => {
+export const Comments = ({ comment, post }) => {
     const [showModal, setShowModal] = useState(false)
     const sessionUser = useSelector(state => state.session.user);
-    
+    function lastUpdated() {
+        let now = new Date();
+        let updatedAt = new Date(comment.updated_at)
+        let difference = (now-updatedAt)/1000/60/60
+        console.log('what is the difference', difference)
+        if(difference > 24){
+            return moment(updatedAt).format("MMMM D YYYY")
+        }else if (difference < 1){
+            if(Math.floor(difference * 60) === 1 ){
+                return `${Math.floor(difference * 60)} minute ago`
+            }else if(difference*60 < 1){
+                return "Less than a minute ago"
+            }
+            return `${Math.floor(difference * 60)} minutes ago`
+        }
+        else if(Math.floor(difference) === 1){
+            return `${Math.floor(difference)} hour ago`
+        }else{
+            return `${Math.floor(difference)} hours ago`
+        }
+    }
+
 
     return (
         <div key={comment.id} className='comments'>
             <img src={comment.users.profile_image} alt="profile pic"></img>
-            <div className='comments-flex'>
-                <Link to={`/users/${comment.users.id}`} className="home-card-username-bottom">{comment.users.username}</Link>
-                <li className="view-comments" key={comment.id}>{comment.content}</li>
-                <li id="date-time">{comment.updated_at}</li>
-                {sessionUser.id === comment.user_id && (
-                    <>
-                        <div onClick={() => setShowModal(true)}><i class="fa-solid fa-ellipsis"></i></div>
+            <div className="comment-card-comment-info">
+                <div className='comments-flex'>
+                    <Link to={`/users/${comment.users.id}`} className="comment-card-username-bottom">{comment.users.username}</Link>
+                    &nbsp;&nbsp;{comment.content}
+                </div>
+                <div className="comment-card-each-bottom-nav">
+                    <div id="date-time">
+                        {lastUpdated()}
+                    </div>
+                    {sessionUser.id === comment.user_id && (
+                        <div className="edit-comment-button-div">
+                            <div className = 'edit-comment-button' onClick={() => setShowModal(true)}>
+                                <i class="fa-solid fa-ellipsis"></i>
+                            </div>
                             {showModal && (
                                 <Modal onClose={() => setShowModal(false)}>
                                     <EditDeleteComment comment={comment} post={post} setShowModal={setShowModal} />
                                 </Modal>
                             )}
-                    </>
-                )}
+                        </div>
+                    )}
+                </div>
             </div>
+
+
         </div>
     )
 }
